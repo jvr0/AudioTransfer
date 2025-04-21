4d8e840e01e647f15302e49f4eb4828494e7746a
joaquin <joaaquinn7@gmail.com>
2025-04-21 14:12:41 +0200

    versión funcional, front listo. Falta refi fetch_data

--- main.py
+++ main.py
@@ -1,1 +1,50 @@
+import streamlit as st
 import pandas as pd
+import time
+from PIL import Image
+from io import BytesIO
+
+from src.support_API import *
+
+# CONFIG INICIAL
+st.set_page_config(
+    page_title="AudioAlchemy",
+    page_icon=":alembic:",
+    layout="wide",
+    initial_sidebar_state="expanded",
+)
+
+identificadores = st.text_area('Ingresa los identificadores (Sin espacios, separados por ",")', height=200)
+if " " in identificadores:
+    st.text("Hay espacios en tu texto")
+identificadores = identificadores.split(',')
+if len(identificadores) > 25:
+    st.text("Más de 25 identificadores añadidos")
+referencia = st.text_input("Ingresa la referencia")
+rows = []  # Lista para guardar los resultados
+
+if st.button(":green[Activar]"):
+
+    for i in identificadores:
+        row = fetch_data(i, referencia)
+        rows.append(row)
+        time.sleep(3)
+
+    df = pd.DataFrame(rows)
+
+    # Guardar el excel en la memoria
+    output = BytesIO()
+    df.to_excel(output, index=False, engine='xlsxwriter')
+    output.seek(0)
+
+    # Botón para descargar
+    st.download_button(
+        label="📥 Descargar resultados",
+        data=output,
+        file_name="csv_TC.xlsx",
+        mime="application/vnd.openxmlformats-officedocument.spreadsheetml.sheet"
+    )
+
+icono = Image.open('img/icono.png')
+st.sidebar.image(icono, width=100)
+st.sidebar.title("AudioTransfer")
--- src/support_API.py
+++ src/support_API.py
@@ -5,6 +5,7 @@ import streamlit as st
 import requests as req
 import os
 import time
+import random
 # Cargar las variables del archivo .env
 load_dotenv()
 
@@ -17,29 +18,31 @@ oauth = OAuth1(
                 verifier=os.getenv('oauth_verifier')
                 )
 
-def fetch_data(id):
+def fetch_data(id, ref_num):
     url = f"https://api.discogs.com/releases/{id}"
     res = req.get(url, auth=oauth)
     data = res.json()
 
-    barcode = next((i.get('value') for i in data.get('identifiers', []) if i.get('description') == 'Scanned'), None )
+    barcode = next((i.get('value') for i in data.get('identifiers', []) if i.get('description') == 'Scanned'), "" )
     artista = data.get('artists_sort')
     titulo = data.get('title', '')
     country = data.get('country', '')
     released = data.get('released', '')
     formato_name = data.get('formats',[])[0].get('name', '')
     formato_description = data.get('formats',[])[0].get('descriptions', '')[0]
+    catno = data.get('labels', '')[0].get('catno', '')
 
     row = {
-        'TÍTULO': f'{artista} - {titulo} {formato_name}',
-        'DESCRIPCIÓN': f"""{artista} - {titulo}
-{formato_name}, {formato_description},
+'TÍTULO': f'{artista} - {titulo} ({formato_name})',
+'DESCRIPCIÓN': f"""{artista} - {titulo}
+{formato_name}, {formato_description}
 {country}, {released}
-Cat. No:
+Cat. No: {catno}
 Barcode: {barcode}""",
-        'REFERENCIA': f'{data.get('identifiers')[1].get('value')}' if len(data.get('identifiers', [])) > 1 else None,
+
+        'REFERENCIA': f'{ref_num}-{str(random.randint(1000,9999))}',
         'PRECIO': '',
-        'ISBN': '',
+        'ISBN': f'{barcode}',
         'SECCIÓN': '453',
         'ESTADO': '5',
         'DESCRIPCIÓN DEL ESTADO': 'Envíos muy rápidos con tarifa plana, combine discos y pague solo por el primer lote.',
@@ -49,21 +52,14 @@ Barcode: {barcode}""",
         'FORMA DE ENVÍO': 'Otros',
         'GASTOS FIJOS': '4,5',
         }
+    
+    print(f"Row from {id} added")
 
     return row
 
+data = [12723436, 3375250, 1117020, 5960365, 12691804, 297641, 682481, 3165]
+
+
 # Llamada de ejemplo
 
-
-data = [12723436, 3375250, 1117020]
-
-rows = []  # Lista para guardar los resultados
-
-for i in data:
-    row = fetch_data(i)
-    rows.append(row)
-    time.sleep(2)
-
-df = pd.DataFrame(rows)
-
-df.to_excel('results.xlsx', index=False)
+"""3375250, 1117020, 5960365, 12691804, 297641, 682481, 3165"""
